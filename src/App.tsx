d61769ffa09d0184883e4d17960db42f7ff44be6
RK <rameshkrishna3888@gmail.com>
2023-12-23 21:44:51 -0500

    react register

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,63 @@
-import Button from "./components/Button";
-import Message from "./Message";
-import ListGroup from "./components/ListGroup";
+// import Button from "./components/Button";
+// import Message from "./Message";
+// import ListGroup from "./components/ListGroup";
 // import Card from "./components/Card";
 import ConfirmModal from "./components/ConfirmModal";
 import { useState } from "react";
-import EmailPasswordForm from "./components/Form";
-import ReportForm from "./components/ReportForm";
+// import EmailPasswordForm from "./components/Form";
+// import ReportForm from "./components/ReportForm";
 import Nav from "./components/BootStrapComponents/Nav";
-import CSVReader from "./components/CSVReader";
+// import CSVReader from "./components/CSVReader";
+import ReactHookExample from "./components/ReactHookExample";
 
 function App() {
-  const [activeItem, setActiveItem] = useState<string>("");
+  // const [activeItem, setActiveItem] = useState<string>("");
   const [selectedDirector, setSelectedDirector] = useState<string>("");
   const [selectedProject, setSelectedProject] = useState<string>("");
   const [markdownResponse, setMarkdownResponse] = useState<string>("");
-  const handleActiveItem = (item: string) => {
-    setActiveItem(item);
-  };
-  const items = [
-    "Ramesh",
-    "Krishna",
-    "Leo",
-    "Puttapaka",
-    "Hyderabad",
-    "Shilpa",
-    "Shivani",
-    "Shireesha",
-  ];
+  // const handleActiveItem = (item: string) => {
+  //   setActiveItem(item);
+  // };
+  // const items = [
+  //   "Ramesh",
+  //   "Krishna",
+  //   "Leo",
+  //   "Puttapaka",
+  //   "Hyderabad",
+  //   "Shilpa",
+  //   "Shivani",
+  //   "Shireesha",
+  // ];
 
-  const handleClick = (item: string) => {
-    console.log(item);
-  };
-  const handleCSVData = (data) => {
-    console.log("got data");
-    console.log(data);
-  };
-  const handleFormSubmit = (director, project, report) => {
-    console.log("Director:", director);
-    console.log("Project:", project);
-    console.log("Markdown Response:", report);
-    setSelectedDirector(director);
-    setSelectedProject(project);
-    setMarkdownResponse(report);
+  // const handleClick = (item: string) => {
+  //   console.log(item);
+  // };
+  // const handleCSVData = (data) => {
+  //   console.log("got data");
+  //   console.log(data);
+  // };
+  // const handleFormSubmit = (
+  //   director: string,
+  //   project: string,
+  //   report: string
+  // ) => {
+  //   console.log("Director:", director);
+  //   console.log("Project:", project);
+  //   console.log("Markdown Response:", report);
+  //   setSelectedDirector(director);
+  //   setSelectedProject(project);
+  //   setMarkdownResponse(report);
+  // };
+  const handleFormSubmit = (data) => {
+    console.log(data.director);
+    setSelectedDirector(data.director);
+    setSelectedProject(data.project);
+    setMarkdownResponse(data.markdownResponse);
   };
   return (
     <div>
       <Nav />
-      <Message />
+      {/* <Message /> */}
       {/* <div>
         <Button link="https://google.com" btxtxt="Google" />
       </div>
@@ -63,7 +74,8 @@ function App() {
       />
 
       {/* <EmailPasswordForm /> */}
-      <CSVReader onFormSubmit={handleFormSubmit} />
+      {/* <CSVReader onFormSubmit={handleFormSubmit} /> */}
+      <ReactHookExample onFormSubmit={handleFormSubmit} />
     </div>
   );
 }
---
+++ src/components/ReactHookExample.tsx
@@ -0,0 +1,99 @@
+import React, { useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { readCSV } from "./test_csv_util.js";
+import MDEditor from "@uiw/react-md-editor";
+
+interface ReactHookExampleProps {
+  onFormSubmit: (data: any) => void;
+}
+
+const ReactHookExample = ({ onFormSubmit }: ReactHookExampleProps) => {
+  const [data, setData] = React.useState([]);
+  const [uniqueDirectors, setUniqueDirectors] = React.useState([]);
+
+  const { register, handleSubmit, watch, setValue } = useForm();
+  const selectedDirector = watch("director");
+  const markdownResponse = watch("markdownResponse");
+
+  useEffect(() => {
+    readCSV("src/components/data/projects.csv", (csvData) => {
+      setData(csvData);
+      const directors = new Set(csvData.map((row) => row.Director));
+      setUniqueDirectors(Array.from(directors));
+    });
+  }, []);
+
+  useEffect(() => {
+    setValue("project", "");
+  }, [selectedDirector, setValue]);
+
+  useEffect(() => {
+    setValue("markdownResponse", markdownResponse);
+  }, [markdownResponse, setValue]);
+
+  const filteredProjects = selectedDirector
+    ? data.filter((row) => row.Director === selectedDirector)
+    : [];
+
+  return (
+    <div className="container mt-4">
+      <div className="card">
+        <div className="card-body">
+          <h2 className="card-title">CSV Reader</h2>
+          <form onSubmit={handleSubmit(onFormSubmit)}>
+            <div className="mb-3">
+              <label htmlFor="directorSelect" className="form-label">
+                Choose a Director:
+              </label>
+              <select
+                id="directorSelect"
+                className="form-select"
+                {...register("director")}
+              >
+                {uniqueDirectors.map((director, index) => (
+                  <option key={index} value={director}>
+                    {director}
+                  </option>
+                ))}
+              </select>
+            </div>
+
+            <div className="mb-3">
+              <label htmlFor="projectSelect" className="form-label">
+                Choose a Project:
+              </label>
+              <select
+                id="projectSelect"
+                className="form-select"
+                {...register("project")}
+              >
+                {filteredProjects.map((row, index) => (
+                  <option key={index} value={row.Project}>
+                    {row.Project}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="mb-3" data-color-mode="light">
+              <label htmlFor="markdownResponse" className="form-label">
+                Markdown Response:
+              </label>
+              <MDEditor
+                id="markdownResponse"
+                value={markdownResponse}
+                onChange={(content) => setValue("markdownResponse", content)}
+                style={{ height: "200px" }} // Adjust height as needed
+              />
+            </div>
+
+            <button type="submit" className="btn btn-primary btn-lg">
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ReactHookExample;
